6095023635f4b1db2e03f7f292e15efc550c0b32
Quin-Riley-A <quin.asselin1@gmail.com>
2022-08-22 23:07:41 -0400

    add error message for invalid selection Co-authored by: Noah Cowan <nsncowan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,23 +5,27 @@ window.onload = function(){
     event.preventDefault();
 
     let aardvark = document.getElementById("aardvark");
-    aardvark.setAttribute("class", "hidden");
     let badger = document.getElementById("badger"); 
-    badger.setAttribute("class", "hidden");
     let flamingo = document.getElementById("flamingo");
+    let error = document.getElementById("error");
+
+    aardvark.setAttribute("class", "hidden");
+    badger.setAttribute("class", "hidden");
     flamingo.setAttribute("class", "hidden");
-    
+    error.setAttribute("class", "hidden");
+
     const userChoice = document.querySelector("input#selection").value;
     if (userChoice === "aardvark") {
-      aardvark.removeAttribute("class", "hidden");
+      aardvark.removeAttribute("class");
     }
-
     else if (userChoice === "badger") {
-      badger.removeAttribute("class", "hidden");
+      badger.removeAttribute("class");
     }
-
     else if (userChoice === "flamingo") {
-      flamingo.removeAttribute("class", "hidden");
+      flamingo.removeAttribute("class");
+    }
+    else {
+      error.removeAttribute("class");
     }
   };
 }; 
